44addc5eb94dbe37cb2a69d8f4dbe6fef851aeee
Cristian Andrei <cristyurs@yahoo.com>
2018-12-03 06:32:30 +0200

    validation for QRBN pieces

--- Chess.Web/Chess/src/app/chess-board/chess-board.service.ts
+++ Chess.Web/Chess/src/app/chess-board/chess-board.service.ts
@@ -1,49 +1,88 @@
-import { Injectable } from "@angular/core";
-import { Subject, Observable } from "rxjs";
-import { Move } from "./move.service";
+import {
+  Injectable
+} from "@angular/core";
+import {
+  Subject,
+  Observable
+} from "rxjs";
+import {
+  Move
+} from "./move.service";
 
-@Injectable({providedIn: 'root'})
+@Injectable({
+  providedIn: 'root'
+})
 export class ChessBoardService {
-    private fenSubject: Subject<string> = new Subject();
+  private fenSubject: Subject < string > = new Subject();
 
-    public draw(fen: string): void {
-        this.fenSubject.next(fen);
-    }
+  public draw(fen: string): void {
+    this.fenSubject.next(fen);
+  }
 
-    public get chessBoardState(): Observable<string> {
-        return this.fenSubject.asObservable();
-    } 
+  public get chessBoardState(): Observable < string > {
+    return this.fenSubject.asObservable();
+  }
 
-    public validateMove(move: string)
-    {
-    return true;        
-    }
-    public makeMove(move: Move,board: any)
-    {
-        //VALIDATE MOVE = CHECK IF THERE ARE'T OTHER PIECES BETWEEN POSITIONS OR THE DESTINATION IS'T OCUPPIED BY THE SAME PLAYER
-        if(move.getElimination())
-        {
-            let boardAfterElim = board.position();
-            boardAfterElim[move.getDestinationString()]=boardAfterElim[move.getOriginString()];
-            delete boardAfterElim[move.getOriginString()];
-            board.position(boardAfterElim,false);
-            console.log(move.getOriginString(),move.getDestinationString())
-        }
-        else{
-        board.move(move.getOriginString()+'-'+move.getDestinationString());
-        }    
-    }
-    public pgnTypeMove(move: string, board: any) : any
-    {
-        let moveArray=move.split(" ");
-        let player='w';
-        for (let item of moveArray.slice(1) ){
-             if(item!=="") {
-             let m = new Move(item);
-             m.calculateOrigin(board,player);
-             this.makeMove(m,board);
-             player='b';
-             }       
+  public validateMove(move: Move, board: any) {
+    if (!move.getElimination()) {
+      if (board.position().hasOwnProperty(move.getDestinationString())) {
+        return false;
+      }
+    } else {
+      if (board.position().hasOwnProperty(move.getDestinationString())) {
+        if (board.position()[move.getDestinationString()]) {
+            if(board.position()[move.getDestinationString()][0]===move.getPlayer())
+            {
+                return false;
             }
+        }
+      }
     }
+    if (move.getPiece() === 'K') {
+      return true;
+    }
+    if (['R', 'Q', 'B'].includes(move.getPiece())) {
+      let f = Number(move.getOrigin().getFile().charCodeAt(0));
+      let r = Number(move.getOrigin().getRank());
+      let f1 = Number(move.getDestination().getFile().charCodeAt(0));
+      let r1 = Number(move.getDestination().getRank());
+      let d = move.getDirection();
+      f += d[0];
+      r += d[1];
+      while (r1 !== r || f1 !== f) {
+        if (board.position().hasOwnProperty(String.fromCharCode(f) + String(r))) {
+          return false;
+        }
+        f += d[0];
+        r += d[1];
+      }
+    }
+
+    return true;
+  }
+  public makeMove(move: Move, board: any) {
+    //VALIDATE MOVE = CHECK IF THERE ARE'T OTHER PIECES BETWEEN POSITIONS OR THE DESTINATION IS'T OCUPPIED BY THE SAME PLAYER
+    if (!this.validateMove(move, board)) return false;
+    if (move.getElimination()) {
+      let boardAfterElim = board.position();
+      boardAfterElim[move.getDestinationString()] = boardAfterElim[move.getOriginString()];
+      delete boardAfterElim[move.getOriginString()];
+      board.position(boardAfterElim, false);
+    } else {
+      board.move(move.getOriginString() + '-' + move.getDestinationString());
+    }
+    return true;
+  }
+  public pgnTypeMove(move: string, board: any): any {
+    let moveArray = move.split(" ");
+    let player = 'w';
+    for (let item of moveArray.slice(1)) {
+      if (item !== "") {
+        let m = new Move(item);
+        m.calculateOrigin(board, player);
+        this.makeMove(m, board);
+        player = 'b';
+      }
+    }
+  }
 }
--- Chess.Web/Chess/src/app/chess-board/move.service.ts
+++ Chess.Web/Chess/src/app/chess-board/move.service.ts
@@ -60,16 +60,17 @@ export class Move {
   private origin: BoardSquare;
   private destination: BoardSquare;
   private elimination: Boolean = false;
+  private direction = [];
+  private player;
   constructor(move: string) {
     if (move[0] === move[0].toUpperCase()) {
       this.piece = move[0];
       move = move.slice(1);
     } else
       this.piece = 'P';
-    if(move[move.length-1]==='+') 
-    {
-        this.elimination = true;
-        move=move.replace('+','');
+    if (move[move.length - 1] === '+') {
+      this.elimination = true;
+      move = move.replace('+', '');
     }
     this.origin = new BoardSquare();
     this.destination = new BoardSquare();
@@ -79,8 +80,24 @@ export class Move {
     IMPLEMENT RECIEVING DESTINATION
     */
   }
+
+  public getPiece(): string {
+    return this.piece;
+  }
   public getElimination(): Boolean {
     return this.elimination;
+  }
+  public getPlayer(){
+    return this.player;
+  }
+  public getDestination(): BoardSquare {
+    return this.destination;
+  }
+  public getOrigin(): BoardSquare {
+    return this.origin;
+  }
+  public getDirection() {
+    return this.direction;
   }
   public getOriginString(): string {
     return String(this.origin.getFile() + this.origin.getRank());
@@ -89,6 +106,7 @@ export class Move {
     return String(this.destination.getFile() + this.destination.getRank());
   }
   public calculateOrigin(board: any, player: string) {
+    this.player=player;
     let position = board.position()
     let currentPiece = player + this.piece;
     // FIX ATTACK ON DIAGONAL FOR PEON
@@ -102,74 +120,136 @@ export class Move {
           if (position[result] == currentPiece) {
             this.origin.setFile(String.fromCharCode(f));
             this.origin.setRank(r);
+            this.direction = item;
+            break;
           }
         }
       }
     }
-    if(["wQ","bQ","wR","bR","wB","bB"].includes(currentPiece)){}
+    if (["wQ", "bQ", "wR", "bR", "wB", "bB"].includes(currentPiece)) {}
     // IMPLEMENT CASE FOR MULTIPLE QUEENS 
-    if(["wQ","bQ"].includes(currentPiece)) {
-      for(let item in position){
-          if(currentPiece===position[item]){
-            let f = Number(this.destination.getFile().charCodeAt(0));
-            let r = Number(this.destination.getRank());
-            let f1 = Number(item[0].charCodeAt(0));
-            let r1 = Number(item[1]);
+    if (["wQ", "bQ"].includes(currentPiece)) {
+      for (let item in position) {
+        if (currentPiece === position[item]) {
+          let f = Number(this.destination.getFile().charCodeAt(0));
+          let r = Number(this.destination.getRank());
+          let f1 = Number(item[0].charCodeAt(0));
+          let r1 = Number(item[1]);
 
-            if(Math.abs(f1-f) === Math.abs(r1-r))
-            {
-              this.origin.setFile(String.fromCharCode(f1));
-              this.origin.setRank(r1);  
-              break;
+          if (Math.abs(f1 - f) === Math.abs(r1 - r)) {
+            this.origin.setFile(String.fromCharCode(f1));
+            this.origin.setRank(r1);
+            if (f > f1) {
+              this.direction.push(1);
+              if (r > r1) {
+                this.direction.push(1);
+              } else {
+                this.direction.push(-1);
+              }
+            } else {
+              this.direction.push(-1);
+              if (r > r1) {
+                this.direction.push(1);
+              } else {
+                this.direction.push(-1);
+              }
             }
-            if(Math.abs(f1-f) ===0  || Math.abs(r1-r) ===0)
+            break;
+          }
+          if (Math.abs(f1 - f) == 0 || Math.abs(r1 - r) == 0) {
+            this.origin.setFile(String.fromCharCode(f1));
+            this.origin.setRank(r1);
+            if (Math.abs(f1 - f) === 0) {
+              if (r1 > r) {
+                this.direction.push(0);
+                this.direction.push(-1);
+              } else {
+                this.direction.push(0);
+                this.direction.push(1);
+              }
+              break;
+            } 
+            else(Math.abs(r1 - r) === 0)
             {
-              this.origin.setFile(String.fromCharCode(f1));
-              this.origin.setRank(r1); 
+              if (f1 > f) {
+                this.direction.push(-1);
+                this.direction.push(0);
+              } else {
+                this.direction.push(1);
+                this.direction.push(0);
+              }
               break;
             }
           }
+        }
       }
     }
     //AGAIN YOU MUST IMPLEMENT THE PART OF THE CONSTRUCTOR FOR RECIVING ORIGIN AS WELL
     //ONLY FOR MULTIPLE ROOKS WITH THE SAME COLOR, UNTIL HAVING 2 ROOKS WITH THE SAME LINE COLOR
-    if(["wB","bB"].includes(currentPiece)) {
-      for(let item in position){
-          if(currentPiece===position[item]){
-            let f = Number(this.destination.getFile().charCodeAt(0));
-            let r = Number(this.destination.getRank());
-            let f1 = Number(item[0].charCodeAt(0));
-            let r1 = Number(item[1]);
+    if (["wB", "bB"].includes(currentPiece)) {
+      for (let item in position) {
+        if (currentPiece === position[item]) {
+          let f = Number(this.destination.getFile().charCodeAt(0));
+          let r = Number(this.destination.getRank());
+          let f1 = Number(item[0].charCodeAt(0));
+          let r1 = Number(item[1]);
 
-            if(Math.abs(f1-f) === Math.abs(r1-r))
-            {
-              this.origin.setFile(String.fromCharCode(f1));
-              this.origin.setRank(r1);  
-              break;
+          if (Math.abs(f1 - f) === Math.abs(r1 - r)) {
+            this.origin.setFile(String.fromCharCode(f1));
+            this.origin.setRank(r1);
+            if (f > f1) {
+              this.direction.push(1);
+              if (r > r1) {
+                this.direction.push(1);
+              } else {
+                this.direction.push(-1);
+              }
+            } else {
+              this.direction.push(-1);
+              if (r > r1) {
+                this.direction.push(1);
+              } else {
+                this.direction.push(-1);
+              }
             }
+            break;
           }
+        }
       }
     }
     //AGAIN YOU MUST IMPLEMENT THE PART OF THE CONSTRUCTOR FOR RECIVING ORIGIN AS WELL
     //THE ROOKS CAND BE ON THE SAME COLOR SQUARES
-    if(["wR","bR"].includes(currentPiece)) {
-      for(let item in position){
-          if(currentPiece===position[item]){
-            let f = Number(this.destination.getFile().charCodeAt(0));
-            let r = Number(this.destination.getRank());
-            let f1 = Number(item[0].charCodeAt(0));
-            let r1 = Number(item[1]);
-
-            if(Math.abs(f1-f) ===0  || Math.abs(r1-r) ===0)
-            {
-              this.origin.setFile(String.fromCharCode(f1));
-              this.origin.setRank(r1); 
-              break;
+    if (["wR", "bR"].includes(currentPiece)) {
+      for (let item in position) {
+        if (currentPiece === position[item]) {
+          let f = Number(this.destination.getFile().charCodeAt(0));
+          let r = Number(this.destination.getRank());
+          let f1 = Number(item[0].charCodeAt(0));
+          let r1 = Number(item[1]);
+          this.origin.setFile(String.fromCharCode(f1));
+          this.origin.setRank(r1);
+          if (Math.abs(f1 - f) === 0) {
+            if (r1 > r) {
+              this.direction.push(0);
+              this.direction.push(-1);
+            } else {
+              this.direction.push(0);
+              this.direction.push(1);
             }
+            break;
           }
+          if (Math.abs(r1 - r) === 0) {
+            if (f1 > f) {
+              this.direction.push(-1);
+              this.direction.push(0);
+            } else {
+              this.direction.push(1);
+              this.direction.push(0);
+            }
+            break;
+          }
+        }
       }
     }
-  
-  
   }
 }
